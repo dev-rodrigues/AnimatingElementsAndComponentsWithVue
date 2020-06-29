8e08bcf16f5e3b8fd7f55c7af9a3d18215e08cf0
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2020-06-29 11:35:19 -0300

    configurações iniciais

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Animações</h1>
+        <p>Treinando transições/animações de elementos/componentes com VueJS.</p>
+      </div>
+    </div>
+    <div class="container"></div>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
